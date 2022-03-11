d808086ed9909f7482352b82a0c9c594bfcc66a7
andjey44 <andjeykh@yandex.ru>
2022-03-11 14:19:08 +0300

    Update
    Last Update

--- first_homework/zadacha.py
+++ first_homework/zadacha.py
@@ -1,7 +1,8 @@
 class PseudoArray:
-    sled = pred = None
+    sled = None
+    pred = None
 
-    def __init__(self, length):
+    def __init__(self, length=0):
         self.dim = length
         self.internal = [None] * length
 
@@ -16,30 +17,21 @@ class PseudoArray:
 
 
 class LinkedArray:
-    sled = pred = None
-
     def __init__(self):
-        self.base = PseudoArray(1)
-        self.base.pred = self.base
+        self.head = PseudoArray()
+        self.head.pred = self.head
         self.length = 0
-
-    def ourind(self):
-        col, leng = 1, self.length
-        while leng - col > 0:
-            leng -= col
-            col *= 2
-        return leng - 1
 
     def append(self, value):
         self.length += 1
-        now = self.base.pred
+        now = self.head.pred
 
-        if self.ourind() == 0 and self.length != 1:
+        if self.ourind() == 0:
             last = PseudoArray(self.length)
             last.internal[0] = value
             now.sled = last
             last.pred = now
-            self.base.pred = last
+            self.head.pred = last
         else:
             now.internal[self.ourind()] = value
 
@@ -47,15 +39,18 @@ class LinkedArray:
         if self.length == 0:
             return 0
 
-        now = self.base.pred
+        now = self.head.pred
         now.internal[self.ourind()] = None
         if self.ourind() == 0:
             now.pred.sled = None
-            self.base.pred = now.pred
+            self.head.pred = now.pred
         self.length -= 1
 
     def get(self, index) -> object:
-        now = self.base.pred
+        if index >= self.length:
+            raise Exception('Такого элемента не существует')
+
+        now = self.head.pred
         col = self.length - self.ourind() - 1
         while col > index:
             now = now.pred
@@ -64,16 +59,31 @@ class LinkedArray:
         return now.internal[index - len(now) + 1]
 
     def repr(self) -> str:
-        now = self.base
-        val = [str(self.base.internal[0])]
+        if self.length == 0:
+            return ''
+
+        now = self.head.sled
+        val = [str(now.internal[0])]
         while now.sled is not None:
             val.append(' ')
             now = now.sled
-            col = len(now) if now.sled is not None else self.ourind() + 1
+            if now.sled is not None:
+                col = len(now)
+            else:
+                col = self.ourind() + 1
             for ind in range(col):
                 val.append(str(now[ind]))
 
-        return ' '.join([i for i in val])
+        return ' '.join([j for j in val])
+
+    def ourind(self):
+        col = 1
+        leng = self.length
+        while leng - col > 0:
+            leng -= col
+            col *= 2
+
+        return leng - 1
 
     def __getitem__(self, index):
         return self.get(index)
@@ -85,11 +95,13 @@ class LinkedArray:
         return self.length
 
 
-arr = LinkedArray()
-for i in range(1, 15):
-    arr.append(i)
-    print(arr)
+la = LinkedArray()
+for i in range(1, 20, 3):
+    la.append(i)
+    print(la)
 
-for i in range(8):
-    arr.delete()
-    print(arr)
+for i in range(5):
+    la.delete()
+    print(la)
+
+print(la[1], la[6])
